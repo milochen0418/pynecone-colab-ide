09336484399c867cc59662a878bf8cf9f09635ff
milochen0418 <milo_chen@mail2000.com.tw>
2023-05-13 10:20:10 +0800

    add install.sh

---
+++ utils/install.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+echo "Node installation procedure"
+npm install -g n
+n 16.6.0
+node --version
+echo "Bun installation procedure"
+curl -fsSL https://bun.sh/install | bash -s -- bun-v0.5.9
+~/.bun/bin/bun --version
+/root/.bun/bin/bun --version
+
+echo "Python pynecone installation procedure"
+pip -q install pynecone==0.1.28
+python --version
+pc version
+
+echo "pynecone-examples dependency installation procedure"
+pip -q install openai
+pip -q install plotly-express
+pip -q install plotly
+pip -q install pandas
+pip -q install googletrans-py==4.0.0
+pip -q install requests>=2.28.1
+pip -q install pytz==2022.7.1
+
+echo "Install localtunnel tool"
+npm -q install localtunnel
+
+echo "Install package for generate Url's QR code"
+pip install pyqrcode
+pip install pypng
+pip install Pillow
+
+
+echo "Install IDE's termianl"
+pip install colab-xterm
