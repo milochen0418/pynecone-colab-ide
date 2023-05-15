0fbe39ef6c684c698228ecc1d6151a76b9d9db93
milochen0418 <milo_chen@mail2000.com.tw>
2023-05-15 12:39:10 +0800

    Lock the version for pc 0.1.28

--- utils/init.sh
+++ utils/init.sh
@@ -3,5 +3,5 @@ cd /content/
 rm -rf ./app
 mkdir ./app
 cd /content/app
-git clone https://github.com/milochen0418/pynecone-colab-ide.git
+git clone -b tag-pc-0-1-28 https://github.com/milochen0418/pynecone-colab-ide.git 
 source ./pynecone-colab-ide/utils/setup_base.sh
