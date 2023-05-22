4a84b28cebc0829944c3864f45f5ce13f03f43a1
milochen0418 <milo_chen@mail2000.com.tw>
2023-05-22 11:50:35 +0800

    utils/open-the-tunnel.sh cached url test on colab

--- utils/open-the-tunnel.sh
+++ utils/open-the-tunnel.sh
@@ -2,7 +2,7 @@
 echo -e "\n\n\n===================\n"
 echo "Setup your environment"
 echo "Open the following two URLs and submit the IP Address" 
-echo "(Or just click the blue button 'Click to Continue' if the page has it)"
+echo "(OR just click the blue button 'Click to Continue' if the page has it)"
 echo "URL A = " $(cat /content/app/front_url)
 echo "URL B = " $(cat /content/app/url)
 echo "IP Address = " $(wget -q -O - ipv4.icanhazip.com)
