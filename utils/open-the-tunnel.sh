bc4ceeac899c4c96be5118e808882bc872c70567
milochen0418 <milo_chen@mail2000.com.tw>
2023-05-22 13:06:05 +0800

    add end line

--- utils/open-the-tunnel.sh
+++ utils/open-the-tunnel.sh
@@ -7,3 +7,4 @@ echo "URL A = " $(cat /content/app/front_url)
 echo "URL B = " $(cat /content/app/url)
 echo "IP Address = " $(wget -q -O - ipv4.icanhazip.com)
 echo "If you don't understand, you can read the document ->  https://hackmd.io/@milochen0418/pynecone-colab-ide-tunnel-setup-doc"
+echo -e "\n\n\n===================\n\n\n"
