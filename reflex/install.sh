8bffbb3b2c10b41be543a048af7005571c290f55
milochen0418 <milo_chen@mail2000.com.tw>
2023-07-06 17:22:48 +0800

    init for reflex

---
+++ reflex/install.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+echo "Node installation procedure"
+npm install -g n
+n 16.8.0
+node --version
+echo "Bun installation procedure"
+curl -fsSL https://bun.sh/install | bash -s -- bun-v0.5.9
+~/.bun/bin/bun --version
+/root/.bun/bin/bun --version
+
+echo "Python pynecone installation procedure"
+pip -q install pynecone==0.1.31
+python --version
+pc version
+
+echo "pynecone-examples dependency installation procedure"
+pip -q install openai
+pip -q install plotly-express
+pip -q install plotly
+pip -q install pandas
+pip -q install googletrans-py==4.0.0
+pip -q install requests>=2.28.1
+pip -q install pytz==2022.7.1
+
+echo "Install localtunnel tool"
+npm -q install localtunnel
+
+echo "Install package for generate Url's QR code"
+pip -q install pyqrcode
+pip -q install pypng
+pip -q install Pillow
+
+
+echo "Install IDE's termianl"
+pip -q install colab-xterm
---
+++ reflex/setup_base.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo "HOME = $HOME"
+echo "Go to the APP_PATH = $PWD"
+rm -rf ./py-webapp
+git clone -b tag-pc-0-1-31 https://github.com/milochen0418/py-webapp.git
+cp ./pynecone-colab-ide/utils/append.py ./
+cp ./pynecone-colab-ide/utils/install.sh ./
+cp ./pynecone-colab-ide/utils/tunnel.sh ./
+cp ./pynecone-colab-ide/utils/qrshare.py ./
+cp ./pynecone-colab-ide/utils/cellwebapp.py ./
+mkdir -p /opt/bin
+ln -s /content/app/pynecone-colab-ide/utils/pcrun.sh /opt/bin/pcrun 
+ln -s /content/app/pynecone-colab-ide/utils/keeprun.sh /opt/bin/keeprun 
