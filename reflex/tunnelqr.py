9ca52b5c4698d30cd513b4d31ec272d512ecbfd8
Milo Chen <milochen0418@users.noreply.github.com>
2023-07-13 13:22:46 +0800

    Pr fix 18 need to fix grammar (#20)

--- reflex/tunnelqr.py
+++ reflex/tunnelqr.py
@@ -1,6 +1,4 @@
 from IPython.display import clear_output, Image, display
-
-
 from pyngrok import ngrok
 import pyqrcode
 import png
@@ -17,7 +15,6 @@ os.environ["API_URL"] = backend_url # setup for backend of reflex app
 tunnels = ngrok.get_tunnels()
 display_str = "backend_url = {}\n, frontend_url = {}\n".format(backend_url, frontend_url)
 
-
 pyqrcode.create(frontend_url).png("frontend-qr-code.png",scale=3)
 pyqrcode.create(backend_url).png("backend-qr-code.png",scale=3)
 frontend_im=Image.open("frontend-qr-code.png")
@@ -33,21 +30,19 @@ print("frontend_url\n {}".format(frontend_url))
 print("frontend QRCode")
 display(frontend_im)
 
+print("""
+There are 3 steps you should do if you want your tunnel works well when you are developing a web app.
+Step 1. Please open backend_url in your browser and click the blue button `Visit Site`
+Step 2. Please open frontend_url in your browser and click the blue button `Visit Site`
+Step 3. Run the following cell, named Terminal, and try to run the snake-game web app
+Step 3.1.  # cd app/examples/snakegame/
+Step 3.2.  # reflex init && reflex run
+Step 3.3.  Use your browser to open frontend_url and play the snake-game. If it works, then you can start to develop.
 
-
-
-print("")
-print("There are 3 steps you should do if if your want your tunnel works well when you developing web app")
-print("Step 1. Please open backend_url in your browser and click the blue button `Visit Site`")
-print("Step 2. Please open frontend_url in your browser and click the blue button `Visit Site`")
-print("Step 3. Run the following cell, named Terminal, and try to run snakegame webapp")
-print("Step 3.1.  # cd app/examples/snakegame/")
-print("Step 3.2.  # reflex init && reflex run")
-print("Step 3.3.  Use your browser to open frontend_url and play snakegame. If it works, then you can start to develope")
-print("")
-print("Troubleshooting:")
-print("If you cannot see switch turn on when click `Run` button, that mean Ngrok detect the IP of your colab runtime and ")
-print("add the stronger constraint to limit your tunnles. That why it doesn't work. The way to solve this issue is to ")
-print("Disconnect and delete runtime. And Reconnect. And you need to reinstall environment again.")
+Troubleshooting:
+If you cannot see the switch turn on when clicking the `Run` button, that means Ngrok detects the IP of your colab runtime and 
+add a more vital constraint to limit your tunnels. That's why it doesn't work. The way to solve this issue is to 
+Disconnect and delete runtime. And Reconnect. And it would be best if you reinstalled the environment again.
+""")
 
 pass
